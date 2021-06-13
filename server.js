b0030602b04fd8af2d237a1179246e04cd27ed50
rajdeep200 <69335542+rajdeep200@users.noreply.github.com>
2021-06-13 14:57:24 +0530

    cors added

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require("express")
+var cors = require('cors')
 const connectDB = require("./config/db")
 const userRoutes = require("./routes/userRouter")
 const questionRoutes = require("./routes/questionRoutes")
 const ansRoutes = require("./routes/ansRouter")
 const app = express()
 require("dotenv").config()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true})); 
 
 connectDB()
 
 app.get("/", (req,res) => {
-    res.send("Welcome")
+    res.send("Rj")
 })
 app.use("/users", userRoutes)
 app.use("/questions", questionRoutes)
